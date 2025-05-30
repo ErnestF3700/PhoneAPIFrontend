966cf1e6a230d332917a348aba41a4663ec547a1
ErnestoCarillo <ernestfurtak@onet.pl>
2025-05-30 10:26:47 +0200

    config,routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './components/product-list.component';
+import { ProductDetailComponent } from './components/product-detail.component';
+import { ProductFormComponent } from './components/product-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/:id', component: ProductDetailComponent },
+  { path: 'create', component: ProductFormComponent },
+  { path: 'edit/:id', component: ProductFormComponent },
+];
